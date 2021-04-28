b0158d6483e1491e831f4785aceaccd9f3eea5f0
Lina <chetverkina.alena@gmail.com>
2021-04-28 18:49:43 +1000

    Update app.vue

--- src/components/app.vue
+++ src/components/app.vue
@@ -33,7 +33,7 @@ export default {
   }),
   methods: {
     search() {
-      const table = new Airtable({ apiKey: "keyzJ1Et2GNfslLSL" });
+      const table = new Airtable({ apiKey: "keybsAede2w1vligD" });
       var base = table.base("appjwctbIDtbNXYea");
 
       base("Tracking")
